c0cb8c3e171b70c7ac069ec1c30ec042afecc6e3
dubemmbah <dubemmbah@gmail.com>
2023-12-12 16:54:46 +0100

    feat: interactive fields

---
+++ interactive-password-field/script.js
@@ -0,0 +1,19 @@
+"use strict";
+const input = document.querySelector(".input__field");
+const inputIcon = document.querySelector(".input__icon");
+
+inputIcon.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  inputIcon.setAttribute(
+    "src",
+    input.getAttribute("type") === "password"
+      ? "./img/eye-close.svg"
+      : "./img/eye.svg"
+  );
+
+  input.setAttribute(
+    "type",
+    input.getAttribute("type") === "password" ? "text" : "password"
+  );
+});
